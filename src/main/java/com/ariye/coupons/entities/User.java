a75e09d71d9fc983a19c038e3307104baa5e2ea5
Ariye Caspi <caspi_j@hotmail.com>
2021-07-12 11:45:23 +0300

    .

--- src/main/java/com/ariye/coupons/entities/User.java
+++ src/main/java/com/ariye/coupons/entities/User.java
@@ -3,17 +3,7 @@ package com.ariye.coupons.entities;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 import com.ariye.coupons.dto.UserLoginData;
 import com.ariye.coupons.exeptions.ApplicationException;
